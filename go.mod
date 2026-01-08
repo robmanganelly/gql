451f07780ced408d2f7c6c16f56aea0a408d80fd
Roberto Manganelly Del Risco <rmangane@akamai.com>
2026-01-08 20:06:06 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rmangane-30120/gql
+
+go 1.21
---
+++ gql.go
@@ -0,0 +1,11 @@
+// Package gql provides a query library for REST APIs that allows for rich querying capabilities.
+// It sits on top of standard REST APIs to allow clients to pass complex filters in a standardized way.
+package gql
+
+import "fmt"
+
+// TestExportable is a test function that prints 'success' to the console.
+// This function is used to verify that the package is correctly imported and working.
+func TestExportable() {
+	fmt.Println("success")
+}
